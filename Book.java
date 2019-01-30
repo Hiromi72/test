7581bc932a5172dfe85654bbd651a691b4b336a8
Hiromi72 <for.job.b72@gmail.com>
2019-01-30 13:32:17 +0900

    Gitのバージョン管理開始（2回目）

---
+++ Book.java
@@ -0,0 +1,12 @@
+public class Book {
+	private String title;
+	private String author;
+	private int price;
+	public Book(String title, String author, int price){
+		this.title = title;
+		this.author = author;
+		this.price = price;]
+		]
+		public void disp(){
+			System.out.printlin();
+		}
